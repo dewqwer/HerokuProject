540f17eb57beac654d1273e6b20f63d486a5d220
dewqwer <natcha.1@mail.kmutt.ac.th>
2020-09-18 22:22:51 +0700

    ใช้คำสั่ง pipenv install -r requirements.txt

--- numshowbandit/settings.py
+++ numshowbandit/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(e$5z0no(b!c7+g^yj!xa^jf2v2c%3o1=*cqm$e=13vsf&x*l-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://write-blog-by-dew.herokuapp.com/']
 
