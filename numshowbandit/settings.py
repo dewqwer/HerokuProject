6d74585ee9fd367c1711ef5d2d8243ffb44bc2e0
dewqwer <natcha.1@mail.kmutt.ac.th>
2020-09-18 19:47:51 +0700

    เพิ่ม host name

--- numshowbandit/settings.py
+++ numshowbandit/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '(e$5z0no(b!c7+g^yj!xa^jf2v2c%3o1=*cqm$e=13vsf&x*l-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://write-blog-by-dew.herokuapp.com/']
 
 
 # Application definition
