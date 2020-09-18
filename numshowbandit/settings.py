eca2632cc67d18eb55b5f9cfec5faa66f68fabb7
dewqwer <natcha.1@mail.kmutt.ac.th>
2020-09-18 19:26:31 +0700

    ลองทำตามเว็บ heroku ใน file setting.py

--- numshowbandit/settings.py
+++ numshowbandit/settings.py
@@ -52,6 +52,8 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,9 +139,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
-# we whitelist localhost:3000 because that's where frontend will be served
-CORS_ORIGIN_WHITELIST = [
-    'https://localhost:3000',
-]
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+
+django_heroku.settings(locals())
