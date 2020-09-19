cad7a3d3f2dd2df50fd7ff4934f2ceadf6bbd472
dewqwer <natcha.1@mail.kmutt.ac.th>
2020-09-19 15:31:46 +0700

    add host local

--- numshowbandit/settings.py
+++ numshowbandit/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '(e$5z0no(b!c7+g^yj!xa^jf2v2c%3o1=*cqm$e=13vsf&x*l-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['write-blog-by-dew.herokuapp.com']
+ALLOWED_HOSTS = ['write-blog-by-dew.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
