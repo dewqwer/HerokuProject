81bbcd7c6ad6629594a932416ddf660b941727a6
dewqwer <natcha.1@mail.kmutt.ac.th>
2020-09-19 12:19:32 +0700

    เปลี่ยน admin.py ลบพวก table ทดสอบออก

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from backend.models import CountRealTime, Faculty, GraduationDetail, Major, FacultyNewLogic, GraduationDetailNewLogic, MajorNewLogic, CountRealTimeNewLogic
+# from backend.models import CountRealTime, Faculty, GraduationDetail, Major, FacultyNewLogic, GraduationDetailNewLogic, MajorNewLogic, CountRealTimeNewLogic
+from backend.models import CountRealTime, Faculty, GraduationDetail, Major
 
 # Register your models here.
 
@@ -8,7 +9,7 @@ admin.site.register(Faculty)
 admin.site.register(GraduationDetail)
 admin.site.register(Major)
 
-admin.site.register(FacultyNewLogic)
-admin.site.register(GraduationDetailNewLogic)
-admin.site.register(MajorNewLogic)
-admin.site.register(CountRealTimeNewLogic)
+# admin.site.register(FacultyNewLogic)
+# admin.site.register(GraduationDetailNewLogic)
+# admin.site.register(MajorNewLogic)
+# admin.site.register(CountRealTimeNewLogic)
